56b110e89769fee3926ab5b80322b4b353445f71
shiruco <maroton621@gmail.com>
2021-12-08 00:38:50 +0900

    initial commit

---
+++ src/sample_assets/src/index.js
@@ -0,0 +1,19 @@
+import { sample } from "../../declarations/sample";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await sample.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
